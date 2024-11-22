161bb5168b3f59463f162926bc4557625f194fed
krystalg80 <krystalsoccer80@gmail.com>
2024-11-22 13:38:11 -0800

    initialization setup part 1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
+
+const store = configureStore()
+
+if (process.env.NODE_ENV !== 'production') { //this should not be exposed in production, only set in development
+  window.store = store;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
