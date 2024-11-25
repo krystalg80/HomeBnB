2bc9ae8146e04f0974b74650632481d7865ef448
krystalg80 <krystalsoccer80@gmail.com>
2024-11-24 17:45:49 -0800

    backend frontend token updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,24 +1,47 @@
 const express = require('express');
+const path = require('path'); // Required to resolve file paths
 const router = express.Router();
 const apiRouter = require('./api');
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+// API Routes
+router.use('/api', apiRouter);
 
+// Welcome message at the root route (can be removed if not needed)
 router.get('/', (req, res) => {
   res.json({ message: 'Welcome to HomeBnB API!' });
 });
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
 
-router.use('/api', apiRouter);
+// Add a XSRF-TOKEN cookie for development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie('XSRF-TOKEN', csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken,
+    });
+  });
+}
+
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve(__dirname, '../../frontend', 'dist')));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+}
 
 module.exports = router;
