f1398c6832e27c2628ecf5f53589c5c126133d27
krystalg80 <krystalsoccer80@gmail.com>
2024-12-02 10:25:13 -0800

    reset migrations

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     url: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false
     },
     preview: {
